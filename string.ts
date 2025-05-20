1ebf1e8e273e84e3c0f820046b15d464bab805aa
unknownsaying <15368104568@189.cn>
2025-05-20 15:30:32 +0800

    Add files via upload

---
+++ string.ts
@@ -0,0 +1,24 @@
+function number(){
+const one = 1;
+const two = 2;
+const three = 3;
+const four = 4;
+const five = 5;
+const six = 6;
+const seven = 7;
+const eight = 8;
+const nine = 9;
+const zero = 0;
+}
+function string(){
+type one = "!"
+type two = "@"
+type three = "#"
+type four = "$"
+type five = "%"
+type six = "^"
+type seven = "&"
+type eight = "*"
+type nine = "("
+type zero = ")"
+}
