3a24475aac0e39b0279f1323319282578978a796
unknownsaying <15368104568@189.cn>
2025-05-20 11:34:06 +0800

    Update abcdef.c

--- Virus/abcdef.c
+++ Virus/abcdef.c
@@ -6,11 +6,3 @@ float abcdef(){
     float e = 5 / 7;
     float f = 6 / 7;
 }
-int ABCDEF(){
-    int A = 142857;
-    int B = 285714;
-    int C = 428571;
-    int D = 571428;
-    int E = 857142;
-    int F = 999999; 
-}
