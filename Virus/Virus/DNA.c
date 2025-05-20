9adad918b8d4803a24df1ebf21eb39efd2a5091b
unknownsaying <15368104568@189.cn>
2025-05-20 15:28:49 +0800

    Delete Virus/Virus directory

--- Virus/Virus/DNA.c
+++
@@ -1,9 +0,0 @@
-#include "abcdef.c"
-void DNA(){
-   int t1; [inline a]
-   int t2; [inline b]
-   int t3; [inline c]
-   int t4; [inline d]
-   int t5; [inline e]
-   int t6; [inline f]
-;}
--- Virus/Virus/RNA.c
+++
@@ -1,8 +0,0 @@
-void RNA(){
-    extern A; float t7;
-    extern B; float t8;
-    extern C; float t9;
-    extern D; float t10;
-    extern E; float t11;
-    extern F; float t0;
-}
